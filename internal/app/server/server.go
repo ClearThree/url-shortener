c8a0d53e9063376e0a00ab7ae83a990b263fa1d9
vvleshchev <vvleshchev@domclick.ru>
2025-03-03 02:56:12 +0300

    Done iter3

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -1,17 +1,26 @@
 package server
 
 import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 
 	"github.com/clearthree/url-shortener/internal/app/handlers"
 )
 
-func Run() error {
+func ShortenURLRouter() chi.Router {
 	var createHandler handlers.CreateShortURLHandler
 	var redirectHandler handlers.RedirectToOriginalURLHandler
 
-	mux := http.NewServeMux()
-	mux.Handle("/", createHandler)
-	mux.Handle("/{id}", redirectHandler)
-	return http.ListenAndServe(`:8080`, mux)
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	//router.Use(middleware.AllowContentType("text/plain"))
+	router.Post("/", createHandler.ServeHTTP)
+	router.Get("/{id}", redirectHandler.ServeHTTP)
+	return router
+}
+
+func Run() error {
+	return http.ListenAndServe(`localhost:8080`, ShortenURLRouter())
 }
---
+++ internal/app/server/server_test.go
@@ -0,0 +1,82 @@
+package server
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func testRequest(t *testing.T, testServer *httptest.Server, method string, path string, payload string) (*http.Response, string) {
+	var body io.Reader
+	if payload != "" {
+		body = strings.NewReader(payload)
+	} else {
+		body = nil
+	}
+	request, err := http.NewRequest(method, testServer.URL+path, body)
+	require.NoError(t, err)
+	request.Header.Add("Content-Type", "text/plain")
+	resp, err := testServer.Client().Do(request)
+	require.NoError(t, err)
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	return resp, string(respBody)
+}
+
+func TestRouter(t *testing.T) {
+	testServer := httptest.NewServer(ShortenURLRouter())
+	defer testServer.Close()
+
+	var tests = []struct {
+		URL       string
+		method    string
+		payload   string
+		want      string
+		status    int
+		preCreate bool
+	}{
+		{
+			URL:       "/",
+			method:    http.MethodPost,
+			payload:   "https://ya.ru",
+			want:      "http://localhost",
+			status:    http.StatusCreated,
+			preCreate: false,
+		},
+		{
+			URL:       "/lele",
+			method:    http.MethodGet,
+			payload:   "",
+			want:      "Short url not found",
+			status:    http.StatusNotFound,
+			preCreate: false,
+		},
+		{
+			URL:       "/",
+			method:    http.MethodGet,
+			payload:   "https://ya.ru",
+			want:      "",
+			status:    http.StatusOK,
+			preCreate: true,
+		},
+	}
+	for _, test := range tests {
+		var URL = test.URL
+		if test.preCreate {
+			preResponse, shortURL := testRequest(t, testServer, http.MethodPost, test.URL, test.payload)
+			preResponse.Body.Close()
+			URL = strings.TrimPrefix(shortURL, "http://localhost:8080")
+		}
+		resp, got := testRequest(t, testServer, test.method, URL, test.payload)
+		resp.Body.Close()
+		assert.Equal(t, test.status, resp.StatusCode)
+		assert.Contains(t, got, test.want)
+	}
+}
